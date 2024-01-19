4aa5e0ec2bdf5d7a2d745073e01e074ea7200db9
tester4223 <156573882+tester4223@users.noreply.github.com>
2024-01-19 09:10:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function formatDateFromString(dateString) {
-        
+        const [day, month] = dateString.split('-');
         return `${day}-${month}`;
     }
 });
